779f787064c9f4dbb7861250d3a2581e1f334162
Tyler Socholotuik <socholotuikt@gmail.com>
2025-01-18 23:28:37 +0000

    created get_movied function

--- api/dao/movies.py
+++ api/dao/movies.py
@@ -22,7 +22,24 @@ class MovieDAO:
     # tag::all[]
     def all(self, sort, order, limit=6, skip=0, user_id=None):
         # TODO: Get list from movies from Neo4j
-        return popular
+        # tag::getMovies[]
+        def get_movies(tx, sort, order, limit, skip, user_id):
+            cypher = """
+                MATCH (m:Movie)
+                WHERE m.`{0}` IS NOT NULL
+                RETURN m {{ .* }} AS movie
+                ORDER BY m.`{0}` {1}
+                SKIP $skip
+                LIMIT $limit
+            """.format(sort, order)
+
+            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id)
+
+            return [row.value("movie") for row in result]
+        # end::getMovies[]
+
+        with self.driver.session() as session:
+            return session.execute_read(get_movies, sort, order, limit, skip, user_id)
     # end::all[]
 
     """
